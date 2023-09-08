f0d6b9a92101b3e331edddfc90cf96b331a9a2db
sosamsony <sosamsonosanyaolu@gmail.com>
2023-09-08 16:45:42 +0400

    Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,11 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
+            <br/>
+            <br/>
+      <br/>
+      <br/>
+      <br/>
       <span>Samsonadmin</span>
       <span>© <a href="https://sosamson.com" target="_blank"
       >Samson Admin Dashboard </a></span>
